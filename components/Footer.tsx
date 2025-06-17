f3317673620d5dfcb0c66dbd4322eed6867dfff5
Mintesnot Afework <MintesnotAfework12@gmail.com>
2025-06-17 10:08:13 +0300

    fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -39,7 +39,7 @@ const Footer = () => {
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-              <a href={info.url} key={info.id}>
+              <a href={info.url} key={info.id} target="_blank">
                 <div
                   key={info.id}
                   className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
